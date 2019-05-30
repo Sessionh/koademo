a0e40951a5c70efab8d86e3016d25ee5b1f40c22
huyh <huyh@51kezhi.com>
2019-05-30 16:20:49 +0800

    first commit

---
+++ config/default.js
@@ -0,0 +1,15 @@
+const config = {
+  // 启动端口
+  port: 1200,
+
+  // 数据库配置
+  database: {
+    DATABASE: 'node',
+    USERNAME: 'root',
+    PASSWORD: '123456',
+    PORT: '3306',
+    HOST: '39.106.12.146'
+  }
+}
+
+module.exports = config
